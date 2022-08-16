ab6903cd6100ed63a208dfd1a252d34c7c3d5650
Alex Kozak <56194044+Alex-byte256@users.noreply.github.com>
2022-08-16 03:32:29 +0300

    creat react app

---
+++ react-chat/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Test from "./components/test.jsx";
+
+function App() {
+  return (
+    <div>
+      Hello World
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-chat/src/components/test.jsx
@@ -0,0 +1,9 @@
+const Test = () => {
+  return (
+    <div>
+      <p>Its work great work</p>
+    </div>
+  );
+};
+
+export default Test;
